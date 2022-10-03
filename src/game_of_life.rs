4af612becf486541dd49721861c40b2132d4ba82
corbo <marwane.grosjacques@ens.uvsq.fr>
2022-10-03 16:33:10 +0200

    struct implementation

--- src/game_of_life.rs
+++ src/game_of_life.rs
@@ -6,6 +6,7 @@ use tui::{
     style::{Color, Style},
     text::Span,
     symbols,
+    Frame,
     Terminal
 };
 use crossterm::{
@@ -16,48 +17,118 @@ use crossterm::{
 const HELP: &str = "[s]: 'swap cell state', [p]: 'pause/resume game', [c]: 'clear grid', [arrows]: 'move cursor'";
 
 
-fn get_next_gen(grid: &Vec<Vec<i8>>) -> Vec<Vec<i8>> {
-    let rows = grid.len();
-    let cols = grid[0].len();
-    let mut next_gen: Vec<Vec<i8>> = vec![vec![0; cols]; rows];
+struct Cursor {
+    x: usize,
+    y: usize,
+}
 
-    for y in 0..rows {
-        for x in 0..cols {
-            let cell_state = grid[y][x];
 
-            // count neighbors \\
-            let mut live_neighbors = -cell_state;
-            for i in -1i16..=1 {
-                for j in -1i16..=1 {
-                    let new_x = (x as i16) + i;
-                    let new_y = (y as i16) + j;
+struct Game {
+    running_time: u16,
+    paused: bool,
+    cols: f64,
+    rows: f64,
+    show_history: bool,
+    cursor: Cursor,
+}
 
-                    if new_x > 0 && new_y > 0 && new_x < cols as i16 && new_y < rows as i16 {
-                        live_neighbors += grid[new_y as usize][new_x as usize];
-                    }
-                }
-            }
 
-            // underpopulation \\
-            if cell_state == 1 && live_neighbors < 2 {
-                next_gen[y][x] = 0;
-            }
-            // overpopulation \\
-            else if cell_state == 1 && live_neighbors > 3 {
-                next_gen[y][x] = 0;
-            }
-            // reproduction \\
-            else if cell_state == 0 && live_neighbors == 3 {
-                next_gen[y][x] = 1;
-            }
-            // stable population \\
-            else {
-                next_gen[y][x] = cell_state;
-            }
+impl Game {
+    fn new(c: f64, r: f64) -> Game {
+        Game {
+            running_time: 0,
+            paused: true, 
+            cols: c,
+            rows: r, 
+            show_history: false, 
+            cursor: Cursor {
+                x: (c as usize)/2,
+                y: (r as usize)/2,
+            }, 
         }
     }
-    // return \\
-    next_gen
+}
+
+
+struct Population {
+    current_generation: Vec<Vec<i8>>,
+    dying_generation: Vec<(f64, f64)>,
+    ghost_generation: Vec<(f64, f64)>,
+    cols: usize,
+    rows: usize,
+}
+
+
+impl Population {
+    fn new(c: usize, r: usize) -> Population {
+        Population {
+            current_generation: vec![vec![0; c]; r],
+            dying_generation: Vec::new(),
+            ghost_generation: Vec::new(),
+            cols: c,
+            rows: r,
+        }
+    }
+
+    fn get_next_gen(&mut self) {
+        let mut next_gen: Vec<Vec<i8>> = vec![vec![0; self.cols]; self.rows];
+
+        for y in 0..self.rows {
+            for x in 0..self.cols {
+                let cell_state = self.current_generation[y][x];
+
+                // count neighbors \\
+                let mut live_neighbors = -cell_state;
+                for i in -1i16..=1 {
+                    for j in -1i16..=1 {
+                        let new_x = (x as i16) + i;
+                        let new_y = (y as i16) + j;
+
+                        if new_x > 0 && new_y > 0 && new_x < self.cols as i16 && new_y < self.rows as i16 {
+                            live_neighbors += self.current_generation[new_y as usize][new_x as usize];
+                        }
+                    }
+                }
+
+                // underpopulation \\
+                if cell_state == 1 && live_neighbors < 2 {
+                    next_gen[y][x] = 0;
+                }
+                // overpopulation \\
+                else if cell_state == 1 && live_neighbors > 3 {
+                    next_gen[y][x] = 0;
+                }
+                // reproduction \\
+                else if cell_state == 0 && live_neighbors == 3 {
+                    next_gen[y][x] = 1;
+                }
+                // stable population \\
+                else {
+                    next_gen[y][x] = cell_state;
+                }
+            }
+        }
+        self.ghost_generation = self.dying_generation.clone();
+        self.dying_generation = Vec::new();
+        for y in 0..self.rows {
+            for x in 0..self.cols {
+                if self.current_generation[y][x] == 1 {
+                    self.dying_generation.push((x as f64, y as f64));
+                }
+            }
+        }
+        self.current_generation = next_gen;
+    }
+
+    fn switch(&mut self, x: usize, y: usize) {
+        self.current_generation[y][x] = 1 - self.current_generation[y][x]
+    }
+
+    fn kill_all(&mut self) {
+        self.current_generation = vec![vec![0; self.cols]; self.rows];
+        self.dying_generation = Vec::new();
+        self.ghost_generation = Vec::new();
+    }
 }
 
 
@@ -65,62 +136,15 @@ pub fn run_gol<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     // grid creation \\
     let (c, r) = size().unwrap();
     let (cols, rows) = ((c-2) as usize, (r-5) as usize);
-    let mut new_gen: Vec<Vec<i8>> = vec![vec![0; cols]; rows];
-    let mut mid_gen: Vec<(f64, f64)> = Vec::new();
-    let mut old_gen: Vec<(f64, f64)> = Vec::new();
+    let mut population = Population::new(cols, rows);
+    let mut game = Game::new(cols as f64, rows as f64);
 
-    let mut paused: bool = true;
-    let mut history_on: bool = false;
     let mut last_tick = Instant::now();
-    let tick_rate = Duration::from_millis(100);
-    let mut frame_rate = Duration::from_millis(400);
-    let mut cursor_x = cols / 2;
-    let mut cursor_y = rows / 2;
-    let mut running_time = 0.0;
-
+    let tick_rate = Duration::from_millis(50);
+    let mut frame_rate: u64 = 400;
+    
     loop {
-        terminal.draw(|f| {
-            let chunks = Layout::default()
-                .constraints([Constraint::Length(3), Constraint::Min(2)])
-                .split(f.size());
-            
-            // controls information \\
-            let paragraph = Paragraph::new(HELP)
-                .block(Block::default().title("[Help]").borders(Borders::ALL));
-            f.render_widget(paragraph, chunks[0]);
-            
-            // canvas \\
-            let pause_color = if paused {Color::Green} else {Color::Green};
-            let title = vec![
-                Span::raw("[Game of Life: "),
-                Span::styled("playing", Style::default().fg(pause_color)),
-                Span::raw(format!("| Timer: {}]", running_time as u16))
-            ];
-            let canvas = Canvas::default()
-                .block(Block::default().title(title).borders(Borders::ALL))
-                .x_bounds([0.0, (cols-1) as f64])
-                .y_bounds([0.0, (rows-1) as f64])
-                .marker(symbols::Marker::Block)
-                .paint(|ctx| {
-                    if history_on {
-                        for (x, y) in old_gen.clone() {
-                            ctx.print(x as f64, y as f64, Span::styled("█", Style::default().fg(Color::Rgb(0, 50, 50))))
-                        }
-                        for (x, y) in mid_gen.clone() {
-                            ctx.print(x as f64, y as f64, Span::styled("█", Style::default().fg(Color::Rgb(0, 100, 100))))
-                        }
-                    }
-                    for y in 0..rows {
-                        for x in 0..cols {
-                            if new_gen[y][x] == 1 {
-                                ctx.print(x as f64, y as f64, Span::styled("█", Style::default().fg(Color::Rgb(0, 255, 255))))
-                            }
-                        }
-                    }
-                    ctx.print(cursor_x as f64, cursor_y as f64, Span::styled("█", Style::default().fg(Color::White)))
-                });
-            f.render_widget(canvas, chunks[1]);
-        })?;
+        terminal.draw(|f| ui(f, &game, &population))?;
 
         // time update \\
         let timeout = tick_rate
@@ -132,50 +156,80 @@ pub fn run_gol<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
             if let Event::Key(key) = event::read()? {
                 match key.code {
                     KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Char('p') => paused = !paused,
-                    KeyCode::Char('h') => history_on = !history_on,
+                    KeyCode::Char('p') => game.paused = !game.paused,
+                    KeyCode::Char('h') => game.show_history = !game.show_history,
+                    KeyCode::Char('n') => if game.paused {population.get_next_gen()},
                     KeyCode::Char('c') => {
-                        new_gen = vec![vec![0; cols]; rows];
-                        paused = true;
-                        running_time = 0.0;
+                        population.kill_all();
+                        game.paused = true;
+                        game.running_time = 0;
                     }
                     KeyCode::Char('s') => {
-                        new_gen[cursor_y][cursor_x] = 1 - new_gen[cursor_y][cursor_x]
+                        population.switch(game.cursor.x, game.cursor.y);
                     }
-                    KeyCode::Enter => {
-                        new_gen[cursor_y][cursor_x] = 1 - new_gen[cursor_y][cursor_x]
-                    }
-                    KeyCode::Left => if cursor_x > 0 {cursor_x -= 1},
-                    KeyCode::Right => if cursor_x < (cols-1) {cursor_x += 1},
-                    KeyCode::Up => if cursor_y < (rows-1) {cursor_y += 1},
-                    KeyCode::Down => if cursor_y > 0 {cursor_y -= 1},
-                    KeyCode::Char('0') => frame_rate = Duration::from_millis(50),
-                    KeyCode::Char('1') => frame_rate = Duration::from_millis(100),
-                    KeyCode::Char('2') => frame_rate = Duration::from_millis(200),
-                    KeyCode::Char('3') => frame_rate = Duration::from_millis(300),
-                    KeyCode::Char('4') => frame_rate = Duration::from_millis(400),
-                    KeyCode::Char('5') => frame_rate = Duration::from_millis(1000),
+                    KeyCode::Enter => population.switch(game.cursor.x, game.cursor.y),
+                    KeyCode::Left => if game.cursor.x > 0 {game.cursor.x -= 1},
+                    KeyCode::Right => if game.cursor.x < game.cols as usize {game.cursor.x += 1},
+                    KeyCode::Up => if game.cursor.y < game.rows as usize {game.cursor.y += 1},
+                    KeyCode::Down => if game.cursor.y > 0 {game.cursor.y -= 1},
+                    KeyCode::Char('+') => if frame_rate < 1000 {frame_rate += 50},
+                    KeyCode::Char('-') => if frame_rate > 50 {frame_rate -= 50},
                     _ => {}
                 }
             }
         }
 
         // grid update \\
-        if last_tick.elapsed() >= frame_rate {
-            if !paused {
-                old_gen = mid_gen.clone();
-                mid_gen = Vec::new();
-                for y in 0..rows {
-                    for x in 0..cols {
-                        if new_gen[y][x] == 1 {
-                            mid_gen.push((x as f64, y as f64));
-                        }
-                    }
-                }
-                new_gen = get_next_gen(&new_gen);
-                running_time += 0.4
+        if last_tick.elapsed() >= Duration::from_millis(frame_rate) {
+            if !game.paused {
+                population.get_next_gen();
+                game.running_time += frame_rate as u16;
             }
             last_tick = Instant::now();
         }
     }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, game: &Game, population: &Population) {
+    let chunks = Layout::default()
+        .constraints([Constraint::Length(3), Constraint::Min(2)])
+        .split(f.size());
+    
+    // controls information \\
+    let paragraph = Paragraph::new(HELP)
+        .block(Block::default().title("[Help]").borders(Borders::ALL));
+    f.render_widget(paragraph, chunks[0]);
+    
+    // canvas \\
+    let pause_span = if game.paused {Span::styled("paused", Style::default().fg(Color::Red))}
+    else {Span::styled("playing", Style::default().fg(Color::Green))};
+    let title = vec![
+        Span::raw("[Game of Life: "),
+        pause_span,
+        Span::raw(format!("| Timer: {}]", game.running_time/1000))
+    ];
+    let canvas = Canvas::default()
+        .block(Block::default().title(title).borders(Borders::ALL))
+        .x_bounds([0.0, game.cols])
+        .y_bounds([0.0, game.rows])
+        .marker(symbols::Marker::Block)
+        .paint(|ctx| {
+            if game.show_history {
+                for (x, y) in population.ghost_generation.clone() {
+                    ctx.print(x, y, Span::styled("█", Style::default().fg(Color::Rgb(0, 50, 50))))
+                }
+                for (x, y) in population.dying_generation.clone() {
+                    ctx.print(x, y, Span::styled("█", Style::default().fg(Color::Rgb(0, 100, 100))))
+                }
+            }
+            for y in 0..(game.rows as usize) {
+                for x in 0..(game.cols as usize) {
+                    if population.current_generation[y][x] == 1 {
+                        ctx.print(x as f64, y as f64, Span::styled("█", Style::default().fg(Color::Rgb(0, 255, 255))))
+                    }
+                }
+            }
+            ctx.print(game.cursor.x as f64, game.cursor.y as f64, Span::styled("█", Style::default().fg(Color::White)))
+        });
+    f.render_widget(canvas, chunks[1]);
 }
